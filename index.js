13176c28653a53882298734c491f3c2858dc3a0f
Aliceson Phillips <alicesonphillips@gmail.com>
2023-07-23 11:37:46 -0400

    wip

--- index.js
+++ index.js
@@ -58,6 +58,7 @@ class Counter {
    */
   constructor(initialNumber) {
     // ✨ initialize whatever properties are needed
+    this.count = initialNumber
   }
 
   /**
@@ -74,6 +75,12 @@ class Counter {
    */
   countDown() {
     // ✨ implement
+    return this.count > 0 ? this.count-- : 0
+    // if(this.count > 0) {
+    //   return this.count--
+    // } else {
+    //   return this.count
+    // }
   }
 }
 
